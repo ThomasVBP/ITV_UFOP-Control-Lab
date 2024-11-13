e2a80dde9cf2be49871f881e823ef6801134f58c
AndreCid <andremacielcid@gmail.com>
2024-11-13 17:54:18 -0300

    MLP Generator class update

--- src/multilayer_perceptron_converter/mlp_generator.py
+++ src/multilayer_perceptron_converter/mlp_generator.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPRegressor
+from sklearn.preprocessing import MinMaxScaler 
 
 def preprocess_and_generate_mlp(input_csv, output_model, test_size=0.2, random_state=42):
     data = pd.read_csv(input_csv)
@@ -14,8 +15,13 @@ def preprocess_and_generate_mlp(input_csv, output_model, test_size=0.2, random_s
         X, y, test_size=test_size, random_state=random_state
     )
 
+    scaler_x = MinMaxScaler()
+    scaler_y = MinMaxScaler()
+    X_train_scaled = scaler_x.fit_transform(X_train)
+    y_train_scaled = scaler_y.fit_transform(y_train.values.reshape(-1, 1)).ravel()
+    
     activation_function = 'tanh'
     mlp = MLPRegressor(hidden_layer_sizes=(2,), activation=activation_function, max_iter=2000, random_state=random_state)
-    mlp.fit(X_train, y_train)
+    mlp.fit(X_train_scaled, y_train_scaled)
     
     return data, mlp, activation_function
